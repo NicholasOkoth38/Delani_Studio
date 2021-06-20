98683fb521e20428cb617a9af054b12d55f840b0
NicholasOkoth38 <nicholas.okoth@student.moringaschool.com>
2021-06-21 01:13:24 +0300

    finalizing js

---
+++ main.js
@@ -0,0 +1,111 @@
+$(document).ready(function () {
+    $(".whatWeDo").addClass("whatWe");
+    $("div.design").click(function () {
+        $("img#design").toggle();
+        $("p.design-text").toggle();
+    });
+
+    $("div.development").click(function () {
+        $("img#development").toggle();
+        $("p.development-text").toggle();
+    });
+
+    $("div.product").click(function () {
+        $("img#product").toggle();
+        $("p.product-text").toggle();
+    });
+
+
+    $(".image").mouseover(function () {
+        $(".image").addClass("imageOpacity");
+    });
+
+    $(".image").mouseout(function () {
+        $(".image").removeClass();
+    });
+
+    $("div.projectOne").mouseover(function () {
+        $("p.project1").addClass(".project1");
+        $(".project1").show();
+    });
+
+    $("div.projectOne").mouseout(function () {
+        $("p.project1").addClass(".project1");
+        $(".project1").hide();
+    });
+
+    $("div.projectTwo").mouseover(function () {
+        $("p.project2").addClass(".project2");
+        $(".project2").show();
+    });
+
+    $("div.projectTwo").mouseout(function () {
+        $("p.project2").addClass(".project2");
+        $(".project2").hide();
+    });
+
+    $("div.projectThree").mouseover(function () {
+        $("p.project3").addClass(".project3");
+        $(".project3").show();
+    });
+
+    $("div.projectThree").mouseout(function () {
+        $("p.project3").addClass(".project3");
+        $(".project3").hide();
+    });
+
+    $("div.projectFour").mouseover(function () {
+        $("p.project4").addClass(".project4");
+        $(".project4").show();
+    });
+
+    $("div.projectFour").mouseout(function () {
+        $("p.project4").addClass(".project4");
+        $(".project4").hide();
+    });
+
+    $("div.projectFive").mouseover(function () {
+        $("p.project5").addClass(".project5");
+        $(".project5").show();
+    });
+
+    $("div.projectFive").mouseout(function () {
+        $("p.project5").addClass(".project5");
+        $(".project5").hide();
+    });
+
+    $("div.projectSix").mouseover(function () {
+        $("p.project6").addClass(".project6");
+        $(".project6").show();
+    });
+
+    $("div.projectSix").mouseout(function () {
+        $("p.project6").addClass(".project6");
+        $(".project6").hide();
+    });
+
+    $("div.projectSeven").mouseover(function () {
+        $("p.project7").addClass(".project7");
+        $(".project7").show();
+    });
+
+    $("div.projectSeven").mouseout(function () {
+        $("p.project7").addClass(".project7");
+        $(".project7").hide();
+    });
+
+    $("div.projectEight").mouseover(function () {
+        $("p.project8").addClass(".project8");
+        $(".project8").show();
+    });
+
+    $("div.projectEight").mouseout(function () {
+        $("p.project8").addClass(".project8");
+        $(".project8").hide();
+    });
+
+});
+
+function reset() {
+    location.reload();
+}
